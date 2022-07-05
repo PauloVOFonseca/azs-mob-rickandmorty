ba6fc282299377c019b1820732e73bb05ada2585
PauloVOFonseca <paulovitoradsifba@gmai.com>
2022-07-05 12:30:53 -0300

    feat: componentize main app

---
+++ lib/presentation/app/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:azship/presentation/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
